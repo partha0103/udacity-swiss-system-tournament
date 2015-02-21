f32a4802026ea8faa522d2dd92d6fa497132f9a4
Sam Frances <sam@samfrances.co.uk>
2015-02-21 23:58:00 +0000

    Add players and matches tables

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,11 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
-
+CREATE TABLE players ( id SERIAL PRIMARY KEY,
+                       name TEXT );
+                       
+CREATE TABLE matches ( player1_id INTEGER REFERENCES players (id),
+                       player2_id INTEGER REFERENCES players (id),
+                       winner_id INTEGER NULL REFERENCES players (id), -- So that draws can be recorded as null
+                       PRIMARY KEY (player1_id, player2_id),
+                       CHECK (player1_id < player2_id) ); -- constraints to ensure that players are matched only once
